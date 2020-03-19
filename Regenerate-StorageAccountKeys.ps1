9b82a1408dc4a819ae9bc885b89f41ca3aacff3d
Carlos Patiño <carlos.patino@catapultsystems.com>
2020-03-19 00:55:57 -0400

    Adding changes to only regenerate 1 key at a time

--- Regenerate-StorageAccountKeys.ps1
+++ Regenerate-StorageAccountKeys.ps1
@@ -2,6 +2,9 @@
     .DESCRIPTION
         Regenerates storage account keys of target Azure storage account.
 
+        Note that an Azure storage account has two keys. This script only regenerates one of the two keys.
+        The parameter $keyNumber specifies whether to regenerate the 1st or the 2nd key.
+
         This PowerSchell script runs as a runbook in an Azure Automation Account, and authenticates to Azure
         through a Service Principal using certificate authentication. This service principal is *not* created
         or configured as part of this script.
@@ -12,13 +15,22 @@
 
     .NOTES
         AUTHOR: Carlos Patiño
-        LASTEDIT: March 16, 2020
+        LASTEDIT: March 19, 2020
 #>
 
 param(
-    $resourceGroupName,
-    $storageAccountName
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroupName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$storageAccountName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('1','2')]
+    [string]$keyNumber
 )
+
+$ErrorActionPreference= "Stop"
 
 $connectionName = "AzureRunAsConnection"
 try
@@ -44,18 +56,19 @@ catch {
     }
 }
 
+# Validate access to the storage account
 Write-Output "Getting key names from storage account [$storageAccountName] in resource group [$resourceGroupName]..."
 $storageAccountKeyNames = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName).KeyName
 Write-Output "Key names acquired. Key 1 Name: [$($storageAccountKeyNames[0])]. Key 2 Name: [$($storageAccountKeyNames[1])]."
 
-Write-Output "Regenerating first key: [$($storageAccountKeyNames[0])]..."
-New-AzStorageAccountKey -ResourceGroupName $resourceGroupName `
-    -Name $storageAccountName `
-    -KeyName $storageAccountKeyNames[0]
-Write-Output "First key: [$($storageAccountKeyNames[0])] regenerated."
+# Determine which key to regenerate
+if ($keyNumber -eq '1') {$index=0}
+elseif ($keyNumber -eq '2') {$index=1}
+else {throw "Error: parameter 'keyNumber' is not valid"}
 
-Write-Output "Regenerating second key: [$($storageAccountKeyNames[1])]..."
+# Regenerate single key
+Write-Output "Regenerating key: [$($storageAccountKeyNames[$index])]..."
 New-AzStorageAccountKey -ResourceGroupName $resourceGroupName `
     -Name $storageAccountName `
-    -KeyName $storageAccountKeyNames[1]
-Write-Output "Second key [$($storageAccountKeyNames[1])] regenerated."
+    -KeyName $storageAccountKeyNames[$index]
+Write-Output "Key [$($storageAccountKeyNames[$index])] regenerated."
